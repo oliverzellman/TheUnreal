21941f9e6af2b8b2e9a19d48667ddba6878a396a
oliverzellman <oliverzellman@gmail.com>
2014-01-06 10:14:22 +0100

    Basic Character Controller and Physics
    Jag har lagt till grundläggande fysik för karaktären gravitation och
    collisions detection. Jag har också fixat så att karaktären kan flytta
    sig längs i sidleds längs x-axeln. Karaktären kan också hoppa längs
    y-axeln.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+	//Player Handling
+	public float gravity = 20; 
+	public float speed = 8;
+	public float acceleration = 30;
+	public float jumpHeight = 12; 
+
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+
+	private PlayerPhysics playerPhysics;
+
+	// Use this for initialization
+	void Start () {
+		playerPhysics = GetComponent<PlayerPhysics> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentSpeed = IncrementTowards (currentSpeed, targetSpeed, acceleration);
+
+		if (playerPhysics.grounded) {
+			amountToMove.y = 0;
+			//Jump
+			if(Input.GetButtonDown("Jump")){
+				amountToMove.y = jumpHeight;
+			}
+		}
+
+		amountToMove.x = currentSpeed;
+		//falling
+		amountToMove.y -= gravity * Time.deltaTime; 
+		playerPhysics.Move (amountToMove * Time.deltaTime);
+	}
+
+	private float IncrementTowards(float n, float target, float a) {
+				if (n == target) {
+						return n;
+				} else {
+						float dir = Mathf.Sign (target - n); // Must n be increased or decreased to get closer to target
+						n += a * Time.deltaTime * dir; 
+						return(dir == Mathf.Sign (target - n)) ? n : target; //If n has now passed target then return target otherwise return n
+				}
+		}
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 s;
+	private Vector3 c;
+
+	private float skin = .005f;
+
+	[HideInInspector]
+	public bool grounded;
+
+	Ray ray;
+	RaycastHit hit;
+
+
+	void Start(){
+		collider = GetComponent<BoxCollider> ();
+		s = collider.size;
+		c = collider.center;
+	}
+	public void Move(Vector2 moveAmount){
+
+	
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position;
+		grounded = false;
+		for (int i=0; i<3; i++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + c.x - s.x/2) + s.x/2 *i; //left, center and then rightmost ContactPoint OffMeshLink collider
+			float y = p.y + c.y + s.y/2 * dir; //Bottom of collider 
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			Debug.DrawRay(ray.origin,ray.direction);
+
+			if(Physics.Raycast(ray, out hit,Mathf.Abs(deltaY),collisionMask)){
+				//Get Distance between player and ground
+				float distance = Vector3.Distance(ray.origin, hit.point);
+
+				// Stop player.s downwards movment after coming within skin width of a collider
+				if(distance > skin) {
+					deltaY = -distance + skin; 
+				}
+				else {
+					deltaY= 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+
+		Vector2 finalTransofrm = new Vector2(deltaX, deltaY);
+
+		transform.Translate (finalTransofrm);
+
+	}
+}
