da0c5e9e19ef6e37fbafd1d59c8fd75f55baab85
Anders <caster_444@hotmail.com>
2014-01-18 12:32:12 +0100

    A* Map
    Added the "aStjärna"-map with some a* features scripted in c#

---
+++ Assets/Scripts/AI_Template.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Pathfinding;
+
+public class AI_Mall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
